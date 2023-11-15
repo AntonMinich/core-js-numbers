588eb12dae352da28a7537e0739f39c8be2d1056
Anton Minich <ant.minich@gmail.com>
2023-11-15 19:19:11 +0300

    refactoring: final task

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -51,8 +51,12 @@ function getCircleCircumference(radius) {
  *  10, 0  => 5
  *  -3, 3  => 0
  */
-function getAverage(/* value1, value2 */) {
-  throw new Error('Not implemented');
+function getAverage(value1, value2) {
+  const average = (value1.MAX_VALUE + value2) / 2;
+  if (isNaN(average)) {
+    return NaN;
+  }
+  return average;
 }
 
 /**
